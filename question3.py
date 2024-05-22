37fcda6cd815341c924a8b9d5dac27993af8efe9
GokulanRS <153983082+GokulanRS@users.noreply.github.com>
2024-05-22 10:55:47 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,10 @@
 #write a program to find the factorial of a nummber
+def fac(num):
+    if(num==1):
+        return 1
+    else:
+        return num * fac(num-1)
+
+num=int(input("Enter any number"))
+x=fac(num)
+print(x)
