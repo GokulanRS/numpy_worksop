a1c6119c6ef88679d7355bb2fc73806e1aaecb33
GokulanRS <153983082+GokulanRS@users.noreply.github.com>
2024-05-22 10:55:08 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,1 +1,11 @@
 # find if the given number is a palindrome or not?
+x=int(input("Enter any number"))
+y=x
+rev=0
+while(y!=0):
+    rev=(rev*10)+(y%10)
+    y=y//10
+if(rev==x):
+    print("PALINDROM")
+else:
+    print("NOT PALINDROM")
